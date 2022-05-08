7a58bb59d41c9a862bf216c671d2f485f69c14c0
Duda <mariaelgalmeida@gmail.com>
2022-05-08 16:28:24 -0700

    Second

--- app/src/main/java/com/example/imunzei_ofc/MainActivity.java
+++ app/src/main/java/com/example/imunzei_ofc/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.imunzei_ofc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.chip.Chip;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        //setContentView(binding.getRoot());
+
+        Chip comecar = (Chip) findViewById(R.id.botao);
+        comecar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(it);
+            }
+        });
     }
 }
